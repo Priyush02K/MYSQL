06065b194234d7063f1c0cd6240a65b92195d4fc
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2024-07-31 22:11:01 +0530

    inset

---
+++ Learn-Concept/insert  into.sql
@@ -0,0 +1,5 @@
+insert into users
+(id, name ,email,password,contact,address,dob,gender,status)
+values
+(2,"priyush","abc@gmailcom","Pass#02","9511689394","Mumbai","1198-04-02","M",1),
+(3,"Shubhausersm","shu@gmailcom","Pass#03","9511686394","Mumbai","1198-6-02","M",1)
